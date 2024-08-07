23251817cfa86645b81172c14cc0b0b429cd10ef
Jordao <jordandhunt01@gmail.com>
2024-08-07 11:12:39 +0100

    fix shop generator bugs

--- src/components/shop/ShopGenerator.jsx
+++ src/components/shop/ShopGenerator.jsx
@@ -1,30 +1,42 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { CartContext } from '../cartContext/CartContext';
 
 const ShopGenerator = ({ items }) => {
   const { updateCartAmount } = useContext(CartContext);
+  const [basket] = useState(() => {
+    const savedBasket = JSON.parse(localStorage.getItem('data')) || [];
+    return savedBasket;
+  });
+
+  useEffect(() => {
+    localStorage.setItem('data', JSON.stringify(basket));
+    updateCartAmount();
+  }, [basket, updateCartAmount]);
 
   return (
     <div className="shop">
-      {items.map((item) => {
-        const { id, name, price, img } = item;
-
-        return (
-          <div className="col-4" key={id}>
-            <div id={`product-id-${id}`} className="item">
-              <a href={`/product/${id}`}>
-                <img width="220" src={img} alt="" />
-                <div className="details">
-                  <h3 className='prodtitle'>{name}</h3>
-                  <div className="price-quantity">
-                    <h2 className='pricelabel'>$ {price}</h2>
+      {items.length === 0 ? (
+        <p>No items available</p>
+      ) : (
+        items.map((item) => {
+          const { id, name, price, img } = item;
+          return (
+            <div className="col-4" key={id}>
+              <div id={`product-id-${id}`} className="item">
+                <a href={`/product/${id}`}>
+                  <img width="220" src={img} alt={name} />
+                  <div className="details">
+                    <h3 className="prodtitle">{name}</h3>
+                    <div className="price-quantity">
+                      <h2 className="pricelabel">${price}</h2>
+                    </div>
                   </div>
-                </div>
-              </a>
+                </a>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })
+      )}
     </div>
   );
 };
