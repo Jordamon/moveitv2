ba0e8daca05230df955a96ecd1ef01756a85f66f
Jordao <jordandhunt01@gmail.com>
2024-08-09 10:04:38 +0100

    alter appjs and add redirect file

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Home from './components/pages/Home';
@@ -6,26 +7,26 @@ import Contact from './components/pages/contact/Contact';
 import Products from './components/pages/products/Products';
 import Cart from './components/pages/cart/Cart';
 import IndividualProduct from './components/pages/individualProduct/IndividualProduct.jsx';
+import './App.css';
 import { CartProvider } from './components/cartContext/CartContext'; 
 import CheckoutPage from './components/pages/checkoutPage/Checkout.jsx';
-import './App.css';
 
 function App() {
   return (
-    <CartProvider>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/Products' element={<Products />} />
-          <Route path='/Contact' element={<Contact />} />
-          <Route path='/Account' element={<Account />} />
-          <Route path='/Cart' element={<Cart />} />
-          <Route path='/CheckoutPage' element={<CheckoutPage />} />
-          <Route path='/product/:id' element={<IndividualProduct />} />
-        </Routes>
-      </Router>
-    </CartProvider>
+    <Router>
+      <Navbar />
+      <CartProvider>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Products' element={<Products />} />
+        <Route path='/Contact' element={<Contact />} />
+        <Route path='/Account' element={<Account />} />
+        <Route path='/Cart' element={<Cart />} />
+        <Route path='/CheckoutPage' element={<CheckoutPage />} />
+        <Route path='/product/:id' element={<IndividualProduct />} />
+      </Routes>
+      </CartProvider>
+    </Router>
   );
 }
 
